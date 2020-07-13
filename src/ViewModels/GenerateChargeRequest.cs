ae9c6bed0787793fbea3dfaddb85688cbe5a72e9
Nilson Almeida <nilson.almeida.jr@gmail.com>
2020-07-13 19:33:23 -0300

    Update GenerateChargeRequest.cs
    add new columns to generate charge request no fee.

--- src/ViewModels/GenerateChargeRequest.cs
+++ src/ViewModels/GenerateChargeRequest.cs
@@ -2,12 +2,15 @@ using Newtonsoft.Json;
 
 namespace PlusUltra.StarkBank.ApiClient.ViewModels
 {
-    public class GenerateChargeRequest
-    {
-        public long Amount { get; set; }
-        [JsonConverter(typeof(DateFormatConverter), "yyyy-MM-dd")]
-        public System.DateTime DueDate { get; set; }
-        public string CustomerId { get; set; }
-        public int Overduelimit => 0;
-    }
+  public class GenerateChargeRequest
+  {
+    public long Amount { get; set; }
+    [JsonConverter(typeof(DateFormatConverter), "yyyy-MM-dd")]
+    public System.DateTime DueDate { get; set; }
+    public string CustomerId { get; set; }
+    public int Overduelimit => 0;
+    public int Fine => 0;
+    public int Interest => 0;
+
+  }
 }
